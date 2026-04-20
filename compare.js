f962def155b731b2dc904b0d7d95343ee225eab8
TheRudraBro <rudramraj22@gmail.com>
2026-04-20 23:35:52 +0600

    Updated

---
+++ compare.js
@@ -0,0 +1,16 @@
+console.log(5>10);
+console.log(5<10);
+console.log(5>=10);
+console.log(5<=10);
+console.log(5==10);
+console.log(5!=10);
+console.log(5===10); //strict equal to (also checks the type)
+console.log(5!==10);
+
+var peyaraPrice=40;
+var applePrice=40;
+
+console.log(peyaraPrice>applePrice);
+console.log(peyaraPrice<applePrice);
+console.log(peyaraPrice>=applePrice);
+console.log(peyaraPrice<=applePrice);
