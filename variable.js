66595a62f3657c4d4e519833b6d3e6db30787ace
TheRudraBro <rudramraj22@gmail.com>
2026-04-20 22:54:24 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,30 @@
+/*
+
+1. variable ==> a container for storing data values
+2. var, let, const ==> keywords used to declare variables in JavaScript
+
+*/
+var sunglassPrice = 90;
+//number type variable
+
+var isExpensive = true;
+//boolean type variable
+
+var myName = "Sabbir";
+//string type variable
+
+
+/*
+
+Naming convention for variable:
+
+1. variable name can contain letters, digits, underscores, and dollar signs
+2. variable name cannot start with a digit
+3. variable name is case-sensitive
+4. variable name should be meaningful and descriptive
+5. variable name cannot be a reserved keyword
+6. variable name should not contain spaces, use camelCase instead
+7. variable name should not be too long or too short
+8. variable name should not contain special characters except for underscores and dollar signs
+9. variable name should not be the same as a built-in object or function name 
+*/
